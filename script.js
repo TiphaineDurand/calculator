bc2093ac3bce039bf90b9dfe1c725c91b9560e85
Tiphaine <85015407+TiphaineDurand@users.noreply.github.com>
2021-10-21 08:47:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,124 @@
+//change theme
+
+function set_theme(theme_name){
+    localStorage.setItem('theme', theme_name);
+    document.documentElement.className = theme_name;
+}
+
+function change_theme(){
+    if (localStorage.getItem('theme') === 'theme_1') {
+        set_theme('theme_2');
+    }
+    else if (localStorage.getItem('theme') === 'theme_2') {
+        set_theme('theme_3');
+    }
+    else if (localStorage.getItem('theme') === 'theme_3') {
+        set_theme('theme_1');
+    }
+}
+
+set_theme('theme_1')
+
+//END change theme
+
+function updateScreen(input){
+    document.getElementById("value").textContent = input;
+}
+
+let display = [];
+let all_values = [];
+let new_value = 0;
+let dot = 0;
+let operator = [];
+let number = 0;
+let j = 0;
+
+
+function calcul(value) {
+    if (value == "RESET") {
+        display = "0";
+        all_values = [];
+        new_value = 0;
+        operator = [];
+        dot = 0;
+    }
+    else if (value == "DEL") {
+        if (display.length == 1)
+            display = "0";
+        else
+            display = display.slice(0, display.length - 1);
+    }
+    else if (value == "=") {
+        new_value = number;
+        all_values[j] = new_value;
+        calculator(all_values, operator);
+        display = "0";
+        all_values = [];
+        new_value = 0;
+        operator = [];
+        dot = 0;
+        j = 0;
+        return;
+    }
+    else if (value == "+" || value == "-" || value == "x" || value == "/") {
+        new_value = number;
+        all_values[j] = new_value;
+        j++;
+        operator += value;
+        dot = 0;
+        updateScreen(display);
+        display = [];
+        return;
+    }
+    else if (value == ".") {
+        if (dot == 1)
+            return;
+        else if (display == 0) {
+            display += "0";
+            display += ".";
+            dot = 1;
+        }
+        else {
+            display += value;
+            dot = 1;
+        }
+    }
+    else if (display == "0") {
+        display = value;
+    }
+    else {
+        if (display.length > 10){
+            updateScreen("Error");
+            return;
+        }
+        display += value;
+    }
+    if (display == "00.")
+        display = "0."
+    updateScreen(display);
+    number = parseFloat(display);
+}
+
+function calculator(all_values, operator) {
+    let i = 0;
+    let result = parseFloat(all_values[0]);
+
+    while (operator[i])
+    {
+        if (operator[i] == "+")
+        result += parseFloat(all_values[i+1]);
+        else if (operator[i] == "-")
+            result -= all_values[i+1];
+        else if (operator[i] == "x")
+            result *= all_values[i+1];
+        else if (operator[i] == "/")
+            result /= all_values[i+1];
+        i++;
+    }
+    let str = result.toString();
+    if (str.length > 10) {
+        updateScreen("Error");
+        return;
+    }
+    updateScreen(str);
+}
